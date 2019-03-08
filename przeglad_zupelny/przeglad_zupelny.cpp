0b9e005868e7e61144b70d95158e04abae19348b
spdpn17kw <kinga_pecherz@wp.pl>
2019-03-08 11:25:57 +0100

    dziala dla 3 maszyn i 4 zadan czyli danych ta000

--- przeglad_zupelny/przeglad_zupelny.cpp
+++ przeglad_zupelny/przeglad_zupelny.cpp
@@ -7,17 +7,27 @@
 using namespace std;
 
 
-int Cmax(int ex[4], int tm1[4], int tm2[4], int tm3[4]) {
-	int i=1, j=1;
-	int Cij = tm1[ex[0]];
-	for (i; i < 4; i++) {
-		int Cj[4];
-		Cj
-	}
+int cmax(int ex[4], int tm1[4], int tm2[4], int tm3[4]) {
+	int Cm1[5], Cm2[5], Cm3[5];
+	Cm1[0] = 0;
+		//pierwsza maszyna
+		for (int j = 1; j <= 4; j++) {
+			Cm1[j] = max(Cm1[j - 1],0) + tm1[ex[j-1]-1];
+		}
+		//druga maszyna
+		for (int j = 1; j <= 4; j++) {
+			Cm2[j] = max(Cm2[j - 1], Cm1[j]) + tm2[ex[j - 1] - 1];
+		}
+		//trzecia maszyna
+		for (int j = 1; j <= 4; j++) {
+			Cm3[j] = max(Cm3[j - 1], Cm2[j]) + tm3[ex[j - 1] - 1];
+		}
+		return Cm3[4];
+	
 }
 int main()
 {
-	int number_of_ex,n_m,ex[100],t_m1[10],t_m2[10],t_m3[10];
+	int number_of_ex,n_m,ex[4],t_m1[10],t_m2[10],t_m3[10];
 	ifstream data("data.txt");
 	data >> number_of_ex;
 	data >> n_m;
@@ -31,7 +41,8 @@ int main()
 		do {
 			for (int i = 0; i < number_of_ex; i++) cout << ex[i]<<' ';
 			int Cmax;
-			Cmax = t_m3[ex[0]]
+			Cmax = cmax(ex,t_m1,t_m2,t_m3);
+			cout << "Cmax= " << Cmax;
 			cout << '\n';
 		} while (next_permutation(ex, ex + number_of_ex));
 }
